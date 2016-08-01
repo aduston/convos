e7f1e668794b31472fa32543fbe980483024bc3f
Adam Duston <aduston@gmail.com>
2016-07-31 23:05:14 -0400

    Added command to run locally to upload to s3

--- data/runlocal.py
+++ data/runlocal.py
@@ -4,14 +4,13 @@ import sys
 import os
 import time
 import boto3
-import re
+import subprocess
 
-if len(sys.argv) < 3:
-    print "Usage: ./runlocal.py <mp3 file> <YYYYMMDD>"
+if len(sys.argv) < 2:
+    print "Usage: ./runlocal.py <mp3 file>"
     exit(0)
 
 f = sys.argv[1]
-timestamp = sys.argv[2]
 
 if not f.endswith('mp3'):
     print "File should end with mp3"
@@ -21,15 +20,17 @@ if not os.path.exists(f):
     print "File {0} doesn't exist".format(f)
     exit(0)
 
-create_time = time.ctime(os.path.getctime(f))
 client = boto3.client('s3')
-s3_key = "{0}.mp3".format(timestamp)
+s3_key = os.path.basename(f)
 
 with open(f, 'r') as fd:
-    print("uploading {0} to s3...".format(f))
+    print("uploading {0} to s3 as {1}".format(f, s3_key))
     client.put_object(
         Bucket='convo-upload',
         ACL='private',
         Body=fd,
         Key=s3_key)
     print("uploaded {0}".format(f))
+
+print("securely deleting {0}".format(f))
+subprocess.call(['srm', f])
